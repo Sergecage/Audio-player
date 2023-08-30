9fcd7385765a932ef725d916a87897928b2050fc
Serge Cage <sergeycage@rocketmail.com>
2023-08-30 17:47:12 +0300

    feat: add toggle event

--- library/main/js/main.js
+++ library/main/js/main.js
@@ -3,3 +3,25 @@ const navbarList = document.querySelector("ul");
 document.addEventListener('click', () => {
     navigation.classList.toggle('nav--active');
 });
+
+const iconLog = document.querySelector(".icon-log");
+const menuLog = document.querySelector(".dropMenu");
+iconLog.addEventListener('mouseover', () => {
+    menuLog.style.display = 'block'
+});
+menuLog.addEventListener('mouseout', () => {
+    menuLog.style.display = 'none'
+});
+
+//carousel event
+/*const buttons = menuLog.querySelector("[carousel-button]");
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const offset = button.dataset.carouselOffset === "next" ? 1 : -1;
+    });
+})*/
+
+const circleButton = document.querySelector("about-slider");
+circleButton.addEventListener("click", () => {
+    circleButton.classList.add("active");
+});
