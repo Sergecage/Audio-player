f877fc0d371219a5c3a907cdcac7e429b102d9e0
Serge Cage <sergeycage@rocketmail.com>
2023-10-14 23:10:05 +0300

    feat: add flip function Sat 14.10.23

--- JS30-part3/js/main.js
+++ JS30-part3/js/main.js
@@ -1,31 +1,80 @@
 const container = document.querySelector('.container');
 const newGame = document.querySelector('.of-game');
-const cards = Array.from(document.querySelector('.card'));
+//const cards = Array.from(document.querySelector('.card'));
 const info = document.querySelector('.game-info');
 const flips = document.querySelector('#flips');
 
 //mix card's order
 class SaveGame {
-    constructor(cards) {
-        this.cardArray = cards;
+    constructor() {
+        this.container = container;
+        this.cards = Array.from(this.container.children);
         this.flips = flips;
+        this.delay = 1000;
+        this.cardMatched = [];
     }
-    /*mixCards() {
-        card.forEach(el => {
-            const randomOrder = Math.floor(Math.random() * card.length) + 1;
+    mixCards() {
+        this.cards.forEach(el => {
+            const randomOrder = Math.floor(Math.random() * cards.length) + 1;
+            el.classList.remove('matched');
     
-            el.classList.remove('visible');
             setTimeout(() => {
                 el.style.order = `${randomOrder}`;
             }, 400);
         });
-    }*/
-    startGame()  {
+    }
+
+    checkCard() {
+        const [firstCard, secondCard] = this.cardMatched;
+
+        this.container.classList.add('clear');
+        if(firstCard.dataset.emoji === secondCard.dataset.emoji) {
+            firstCard.classList.replace('visible', 'matched');
+            secondCard.classList.replace('visible', 'matched');
+
+            this.cardMatched = [];
+
+            setTimeout(() => {
+                const perfectMatch = this.cards.every(ele => (
+                    ele.classList.contains('matched')
+                ));
+
+                this.container.classList.remove('clear');
+
+                if(perfectMatch) {
+                    this.mixCards();
+                }
+            }, this.delay);
+        }
+        else {
+            setTimeout(() => {
+                firstCard.classList.remove('visible');
+                secondCard.classList.remove('visible');
+
+                this.cardMatched = [];
+
+                this.container.classList.remove('clear');
+            }, this.delay);
+        }
+    }
+    flipCard(card){
+        card.classList.add('visible');
+        this.cardMatched.push(card);
+
+        if (this.cardMatched.length === 2) {
+            this.checkCard();
+        }
+        /*this.checkCard(card)
+            this.totalFlips++;
+            this.flips.innerText =this.totalFlips;*/
+
+    }
+    /*startGame()  {
         this.cardCheck = null;
         this.totalFlips = 0;
         this.cardMatched = [];
         this.busy = true;
-        this.flips = document.querySelector('#flips');
+        //this.flipTimes = ;
     }
     flipCard(card) {
         if(this.checkFlip(card)){
@@ -37,28 +86,32 @@ class SaveGame {
     checkFlip(card) {
         return true;
         //return !this.busy && !this.cardMatched.includes(card) && card !==this.cardCheck;
-    }
-    
-}
+    }*/
 
+};
+
+const game = new SaveGame;
+
+game.cards.forEach(el => {
+    el.addEventListener('click', game.flipCard.bind(game, el));
+});
 
 //onload page
 const onLoad = () => {    
-    //let newGame = Array.from(document.querySelector('.of-game'));
-    let game = new SaveGame(100, cards);
+    //let game = new SaveGame(100, cards);
     
     
     //newGame.forEach(ele => {
         newGame.addEventListener('click', () => {
         newGame.classList.remove('visible');
-         game.startGame();
+        // game.startGame();
      });
    // });
-    cards.forEach(el => {
+    /*cards.forEach(el => {
         el.addEventListener('click', () => {
             game.flipCard(card);
         });
-    });
+    });*/
 };
 
 if(document.readyState === 'loading') {
